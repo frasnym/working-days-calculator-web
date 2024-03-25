9e4ecd524ad96b61bd890080cf9bc96dd11e16bb
Nyoman Frastyawan <nyoman.frastyawan@amartha.com>
2024-03-25 11:44:35 +0800

    feat(calculator-ui): Add reset button

--- app/page.tsx
+++ app/page.tsx
@@ -80,12 +80,24 @@ export default function Home() {
               required
             />
           </div>
-          <button
-            type="submit"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            Submit
-          </button>
+          <div className="flex space-x-4 ">
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+            <button
+              type="button"
+              className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+              onClick={() => {
+                setInputDateStr("");
+                setWorkingDays("");
+              }}
+            >
+              Reset
+            </button>
+          </div>
         </form>
       )}
     </main>
