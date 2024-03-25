372bdc9adf131ac902b74b2eda6dd5dc37ebd9a9
Nyoman Frastyawan <nyoman.frastyawan@amartha.com>
2024-03-25 11:47:17 +0800

    docs(calculator-ui): Add TODO comment

--- app/page.tsx
+++ app/page.tsx
@@ -41,6 +41,7 @@ export default function Home() {
     }
   };
 
+  // TODO: Move to components
   const loadingEl = (
     <div className="flex items-center justify-center w-full h-full border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
       <div className="px-3 py-1 text-xs font-medium leading-none text-center text-blue-800 bg-blue-200 rounded-full animate-pulse dark:bg-blue-900 dark:text-blue-200">
@@ -49,12 +50,14 @@ export default function Home() {
     </div>
   );
 
+  // TODO: Create larger form size
   return (
     <main className="flex items-center justify-center h-screen">
       {isLoading ? (
         loadingEl
       ) : (
         <form className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+          {/* TODO: Refactor components */}
           <div className="mb-5">
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
               Date Start
@@ -81,6 +84,7 @@ export default function Home() {
             />
           </div>
           <div className="flex space-x-4 ">
+            {/* TODO: Refactor components */}
             <button
               type="submit"
               className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
